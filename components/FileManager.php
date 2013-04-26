97aed808ac53a84e9add99631ddb18fc43a3230b
Christoffer Niska <ChristofferNiska@gmail.com>
2013-04-27 00:23:35 +0300

    Initial commit

---
+++ components/FileManager.php
@@ -0,0 +1,150 @@
+<?php
+/**
+ * FileManager class file.
+ * @author Christoffer Niska <christoffer.niska@gmail.com>
+ * @copyright Copyright &copy; Christoffer Niska 2013-
+ * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
+ * @package crisu83.yii-filemanager.components
+ */
+
+Yii::import('vendor.crisu83.yii-extension.behaviors.ComponentBehavior', true/* force include */);
+
+use crisu83\yii_extension\behaviors\ComponentBehavior;
+
+/**
+ * Application component for managing files.
+ */
+class FileManager extends CApplicationComponent
+{
+	/**
+	 * @var string the path for storing the files.
+	 */
+	public $filePath = 'webroot.files';
+
+	private $_filePath;
+
+	/**
+	 * Initializes the component.
+	 */
+	public function init()
+	{
+		parent::init();
+		$this->attachBehavior('ext', new ComponentBehavior);
+		$this->createPathAlias('fileManager', __DIR__ . DIRECTORY_SEPARATOR . '..');
+		$this->import('models.*');
+	}
+
+	/**
+	 * Saves the given file both in the database and on the hard disk.
+	 * @param CUploadedFile $file the uploaded file.
+	 * @param string $name the new name for the file.
+	 * @param string $path the path relative to the base path.
+	 * @throws CException if saving the image fails.
+	 * @return File the model.
+	 */
+	public function save($file, $name = null, $path = null)
+	{
+		if (!$file instanceof CUploadedFile)
+			throw new CException('Failed to save file. File is not an instance of CUploadedFile.');
+		/* @var CDbConnection $db */
+		$db = $this->getDbConnection();
+		$trx = $db->beginTransaction();
+		try
+		{
+			$model = new File;
+			$model->extension = strtolower($file->getExtensionName());
+			$model->filename = $file->getName();
+			$model->mimeType = $file->getType();
+			$model->byteSize = $file->getSize();
+			$model->createdAt = date('Y-m-d H:i:s');
+			if ($name === null)
+			{
+				$filename = $model->filename;
+				$name = substr($filename, 0, strrpos($filename, '.'));
+			}
+			$model->name = $this->normalizeFilename($name);
+			if ($path !== null)
+				$model->path = trim($path, '/');
+			if ($model->save() === false)
+				throw new CException('Failed to save file. Database record could not be saved.');
+			$filePath = $this->getFilePath() . $model->resolvePath();
+			if (!file_exists($filePath) && !$this->createDirectory($filePath))
+				throw new CException('Failed to save file. Directory could not be created.');
+			$filePath .= $model->resolveFilename();
+			if ($file->saveAs($filePath) === false)
+				throw new CException('Failed to save file. File could not be saved.');
+			$trx->commit();
+			return $model;
+		}
+		catch (\CException $e)
+		{
+			$trx->rollback();
+			throw $e;
+		}
+	}
+
+	/**
+	 * Returns the file with the given id.
+	 * @param integer $id the model id.
+	 * @return File the model.
+	 * @throws CException if loading the file fails.
+	 */
+	public function load($id)
+	{
+		$model = File::model()->findByPk($id);
+		if ($model === null)
+			throw new CException('Failed to load file. Database record not found.');
+		return $model;
+	}
+
+	/**
+	 * Deletes a file with the given id.
+	 * @param integer $id the file id.
+	 * @return boolean whether the file was successfully deleted.
+	 * @throws CException if deleting the file fails.
+	 */
+	public function delete($id)
+	{
+		$model = $this->load($id);
+		$filePath = $model->resolveFilePath();
+		if (file_exists($filePath) !== false && unlink($filePath) === false)
+			throw new CException('Failed to delete file. File could not be deleted.');
+		if ($model->delete() === false)
+			throw new CException('Failed to delete file. Database record could not be deleted.');
+		return true;
+	}
+
+	/**
+	 * Returns the path for storing files.
+	 * @return string the path.
+	 */
+	public function getFilePath()
+	{
+		if (isset($this->_filePath))
+			return $this->_filePath;
+		else
+			return $this->_filePath = Yii::getPathOfAlias($this->filePath) . '/';
+	}
+
+	/**
+	 * Creates a directory.
+	 * @param string $path the directory path.
+	 * @param integer $mode the chmod mode (ignored on windows).
+	 * @param boolean $recursive whether to create the directory recursively.
+	 * @return boolean whether the directory was created.
+	 */
+	protected function createDirectory($path, $mode = 0777, $recursive = true)
+	{
+		return mkdir($path, $mode, $recursive);
+	}
+
+	/**
+	 * Normalizes the given filename by removing illegal characters.
+	 * @param string $name the filename.
+	 * @return string the normalized filename.
+	 */
+	protected function normalizeFilename($name)
+	{
+		return str_replace(str_split('-/\?%*:|"<>. '), '', $name);
+	}
+}
---
+++ models/File.php
@@ -0,0 +1,130 @@
+<?php
+/**
+ * File class file.
+ * @author Christoffer Niska <christoffer.niska@gmail.com>
+ * @copyright Copyright &copy; Christoffer Niska 2013-
+ * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
+ * @package crisu83.yii-filemanager.models
+ */
+
+/**
+ * This is the model class for table "file".
+ *
+ * The followings are the available columns in table 'file':
+ * @property string $id
+ * @property string $name
+ * @property string $extension
+ * @property string $path
+ * @property string $filename
+ * @property string $mimeType
+ * @property string $byteSize
+ * @property string $createdAt
+ */
+class File extends CActiveRecord
+{
+	/**
+	 * Returns the static model of the specified AR class.
+	 * @param string $className active record class name.
+	 * @return File the static model class
+	 */
+	public static function model($className = __CLASS__)
+	{
+		return parent::model($className);
+	}
+
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'file';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		return array(
+			array('name, extension, filename, mimeType, byteSize, createdAt', 'required'),
+			array('name, path, extension, filename, mimeType', 'length', 'max' => 255),
+			array('byteSize', 'length', 'max' => 10),
+			// The following rule is used by search().
+			array('id, name, path, extension, filename, mimeType, byteSize, createdAt', 'safe', 'on' => 'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		return array();
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'id' => Yii::t('label', 'ID'),
+			'name' => Yii::t('label', 'Name'),
+			'path' => Yii::t('label', 'Path'),
+			'extension' => Yii::t('label', 'Extension'),
+			'filename' => Yii::t('label', 'Filename'),
+			'mimeType' => Yii::t('label', 'Mime Type'),
+			'byteSize' => Yii::t('label', 'Byte Size'),
+			'createdAt' => Yii::t('label', 'Created At'),
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		$criteria = new CDbCriteria;
+
+		$criteria->compare('id', $this->id);
+		$criteria->compare('name', $this->name, true);
+		$criteria->compare('path', $this->path, true);
+		$criteria->compare('extension', $this->extension, true);
+		$criteria->compare('filename', $this->filename, true);
+		$criteria->compare('mimeType', $this->mimeType, true);
+		$criteria->compare('byteSize', $this->byteSize, true);
+		$criteria->compare('createdAt', $this->createdAt, true);
+
+		return new CActiveDataProvider($this, array(
+			'criteria' => $criteria,
+		));
+	}
+
+	/**
+	 * Returns the full filename for this file.
+	 * @return string the filename.
+	 */
+	public function resolveFilename()
+	{
+		return $this->name . '.' . $this->extension;
+	}
+
+	/**
+	 * Returns the relative path for this file.
+	 * @return string the path.
+	 */
+	public function resolvePath()
+	{
+		return $this->path !== null ? $this->path . '/' : '';
+	}
+
+	/**
+	 * Returns the full path with the filename for this file.
+	 * @return string the path.
+	 */
+	public function resolveFilePath()
+	{
+		return $this->resolvePath() . $this->resolveFilename();
+	}
+}
