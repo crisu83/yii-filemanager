71cc5bd8b944a3f734eaaace5a05251dbd4a91f9
Crisu83 <ChristofferNiska@gmail.com>
2013-09-05 14:30:04 +0300

    Add unit tests

---
+++ tests/unit/FileManagerTest.php
@@ -0,0 +1,107 @@
+<?php
+
+use Codeception\Util\Stub;
+
+class FileManagerTest extends \YiiTest
+{
+   /**
+    * @var \CodeGuy
+    */
+    protected $codeGuy;
+
+    protected function _before()
+    {
+        $this->mockApplication(
+            array(
+                'aliases' => array(
+                    'project' => __DIR__ . '/../..',
+                    'vendor' => 'project.vendor',
+                ),
+                'components' => array(
+                    'fileManager' => array(
+                        'class' => 'project.components.FileManager',
+                        'baseUrl' => '/test',
+                    ),
+                ),
+                'preload' => array(
+                    'fileManager',
+                ),
+            )
+        );
+    }
+
+    public function testInit()
+    {
+        $this->assertNotEquals(false, Yii::getPathOfAlias('fileManager'));
+        $this->assertTrue(class_exists('File'));
+    }
+
+    public function testCreateModel()
+    {
+        // todo: implement
+    }
+
+    public function testSaveModel()
+    {
+        // todo: implement
+    }
+
+    public function testLoadModel()
+    {
+        // todo: implement
+    }
+
+    public function testDeleteModel()
+    {
+        // todo: implement
+    }
+
+    public function testSendFile()
+    {
+        // todo: implement
+    }
+
+    public function testXSendFile()
+    {
+        // todo: implement
+    }
+
+    public function testGetBasePath()
+    {
+        $manager = $this->getManager();
+        $this->assertEquals('files/', $manager->getBasePath());
+        $this->assertNotEquals(false, strpos($manager->getBasePath(true), 'yii-filemanager/tests/files'));
+    }
+
+    public function testGetBaseUrl()
+    {
+        $manager = $this->getManager();
+        $this->assertEquals('files/', $manager->getBaseUrl());
+        $this->assertEquals('test/files/', $manager->getBaseUrl(true));
+    }
+
+    public function testCreateDirectory()
+    {
+        // todo: implement
+    }
+
+    public function testDeleteDirectory()
+    {
+        // todo: implement
+    }
+
+    public function testNormalizeFilename()
+    {
+        $manager = $this->getManager();
+        $this->assertEquals('test.txt', $manager->normalizeFilename('test/\?%*:|"<>.txt'));
+        $this->assertEquals('test-1.png', $manager->normalizeFilename('test 1.png'));
+    }
+
+    /**
+     * @return FileManager
+     */
+    protected function getManager()
+    {
+        return Yii::app()->getComponent('fileManager');
+    }
+}
